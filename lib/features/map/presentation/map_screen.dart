e406ff5386cc29f8ff606feedd014a1a11caaf87
23aneessss <oa_bouziani@esi.dz>
2026-05-14 00:34:38 +0100

    Add Go Router import to MapScreen: enable routing functionality

--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:go_router/go_router.dart';
 import '../../../core/constants/app_animations.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_sizes.dart';
